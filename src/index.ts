11e1000ec10d414f67fed6083ccbdbc80e76cbd2
special-k <noyabr-web@ya.ru>
2024-02-17 08:36:35 +0500

    remove dev panel

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const createWindow = (): void => {
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
 
     ipcMain.on('score:time', (_: IpcMainEvent, value: string) => {
